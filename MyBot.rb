c246a10a90b6df02d423a111550897476de850e5
Rob vanbrandenburg <robvanb@gmail.com>
2011-11-03 20:42:38 -0400

    Added colission prevention as per tutorial.

--- MyBot.rb
+++ MyBot.rb
@@ -1,7 +1,6 @@
 $:.unshift File.dirname($0)
 require 'ants.rb'
 
-
 ai=AI.new
 
 ai.setup do |ai|
@@ -9,14 +8,26 @@ ai.setup do |ai|
 end
 
 ai.run do |ai|
-	# your turn code here
+	#Prevent collisions
+  @orders = Hash.new            #Gets initialized 'per ant (curAnt) - need to figure out why - works fine though
+	def doMove(curAnt, direction)
+    newLoc = curAnt.square.neighbor(direction)
+	  oldLoc = curAnt
+	  if (curAnt.square.neighbor(direction).land? && !@orders.has_key?(newLoc) )
+      curAnt.order(direction)
+      @orders[newLoc] = oldLoc	
+	    return true
+	  else
+	    return false
+	  end
+	end
 	
+	#Default move
 	ai.my_ants.each do |ant|
 		# try to go north, if possible; otherwise try east, south, west.
 		[:N, :E, :S, :W].each do |dir|
-			if ant.square.neighbor(dir).land?
-				ant.order dir
-				break
+			if doMove(ant, dir)
+			 break
 			end
 		end
 	end
